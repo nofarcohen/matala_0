4f8d8a3a7d89992903dbadef827fc12abf45da6e
nofar-cohen <nofarcohen222@gmail.com>
2021-03-11 21:35:15 +0200

    “new”

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-number1= input("enter number_1_1:")
-number2= input("enter number_2_2:" )
+number1= input("enter number_1:")
+number2= input("enter number_2:" )
 sum= int(number1)+int(number2)
 print("the sum is: ", sum)
